f3eb858a928e5523996058ee0bdd7d5f11b63c98
yasuaki640 <yasuaki640@gmail.com>
2021-02-03 08:41:16 +0900

    仮実装

--- src/test/java/com/example/tddstudy/MoneyTests.java
+++ src/test/java/com/example/tddstudy/MoneyTests.java
@@ -57,4 +57,12 @@ public class MoneyTests {
         Money result = bank.reduce(Money.doller(1), "USD");
         assertEquals(Money.doller(1), result);
     }
+
+    @Test
+    public void testReduceMoneyDifferentCurrency() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.doller(1), result);
+    }
 }
