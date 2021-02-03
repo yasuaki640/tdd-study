759ba0236fc74de2c1898c7ac3d6dc573e9e169b
yasuaki640 <yasuaki640@gmail.com>
2021-02-03 22:48:49 +0900

    扱う方を一般化する

--- src/main/java/com/example/tddstudy/Expression.java
+++ src/main/java/com/example/tddstudy/Expression.java
@@ -1,6 +1,7 @@
 package com.example.tddstudy;
 
 public interface Expression {
+    Expression plus(Expression addend);
 
     public Money reduce(Bank bank, String to);
 }
--- src/main/java/com/example/tddstudy/Money.java
+++ src/main/java/com/example/tddstudy/Money.java
@@ -9,7 +9,7 @@ class Money implements Expression {
         this.currency = currency;
     }
 
-    Money times(int multiplier) {
+    Expression times(int multiplier) {
         return new Money(amount * multiplier, currency);
     }
 
@@ -35,7 +35,7 @@ class Money implements Expression {
         return amount + " " + currency;
     }
 
-    Expression plus(Money addend) {
+    public Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
 
